b47ea298cf8210c86ea50eec563714e969fa6caf
Fidelya1988 <oleksandram1988@gmail.com>
2021-05-19 19:43:16 +0300

    времененно вынесла из стейта данные в презентационную компоненту, закоментировала strictMode в index.js

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,7 +2,26 @@ import React from 'react';
 import styles from './Users.module.css';
 
 const Users = (props) => {
- 
+    if(props.users.length === 0) {
+        
+        props.setUsers(
+           [ {
+                id: 1, followed: true, fullname: 'Volodymyr K', status: 'My life is good', location: { city: 'Kyiv', country: 'Ukraine' },
+                img: 'https://forum.survarium.com/ru/images/avatars/gallery/personnages/avatar1.jpg',
+            },
+            {
+                id: 2, followed: false, fullname: 'Valeria Marchenko', status: 'I love music', location: { city: 'Kyiv', country: 'Ukraine' },
+                img: 'https://image.freepik.com/free-vector/illustration-of-young-woman-meditating_23-2148513213.jpg'
+            },
+            {
+                id: 3, followed: false, fullname: 'Stasya Synko <3', status: 'Hi!!', location: { city: 'Kyiv', country: 'Ukraine' },
+                img: 'https://i.pinimg.com/736x/4a/bc/c0/4abcc00427dbb86ee5da8270b52204f8.jpg'
+            }
+           ])
+    } else {
+        console.log(props.users.length)
+    }
+  
     return (
 
         <div>
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -19,18 +19,7 @@ export const setUsersAC = users => ({
 
 let intialState = {
     users: [
-        {
-            id: 1, followed: true, fullname: 'Volodymyr K', status: 'My life is good', location: { city: 'Kyiv', country: 'Ukraine' },
-            img: 'https://forum.survarium.com/ru/images/avatars/gallery/personnages/avatar1.jpg',
-        },
-        {
-            id: 2, followed: false, fullname: 'Valeria Marchenko', status: 'I love music', location: { city: 'Kyiv', country: 'Ukraine' },
-            img: 'https://image.freepik.com/free-vector/illustration-of-young-woman-meditating_23-2148513213.jpg'
-        },
-        {
-            id: 3, followed: false, fullname: 'Stasya Synko <3', status: 'Hi!!', location: { city: 'Kyiv', country: 'Ukraine' },
-            img: 'https://i.pinimg.com/736x/4a/bc/c0/4abcc00427dbb86ee5da8270b52204f8.jpg'
-        }
+       
     ]
 }
 
